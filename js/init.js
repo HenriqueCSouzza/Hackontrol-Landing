c28b0e89a0a9d8d7d5788b3013f924cf81d74578
HenriqueCSouzza <henrique.csouzza@gmail.com>
2019-09-19 17:58:09 -0300

    Slap2

--- js/init.js
+++ js/init.js
@@ -1,8 +1,28 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+  $(".carousel").carousel();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+  $(".materialboxed").materialbox();
+});
